09047a3287bdb913c47afac1e1f10a2b597bc3af
Rodrigo Marques da Cruz <rodrigo.apostolo@gmail.com>
2016-03-11 22:40:14 -0300

    37

--- Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -18,5 +18,49 @@ namespace Quiron.LojaVirtual.Dominio.Repositorio
         }
 
 
+
+
+        //Salvar Produto - Alterar Produto
+        public void Salvar(Produto produto)
+        {
+            if (produto.ProdutoId == 0)
+            {
+                //Salvado
+                _context.Produtos.Add(produto);
+            }
+            else
+            {
+                //Alteração
+                Produto prod = _context.Produtos.Find(produto.ProdutoId);
+
+                if (prod != null)
+                {
+                    prod.Nome = produto.Nome;
+                    prod.Descricao = produto.Descricao;
+                    prod.Preco = produto.Preco;
+                    prod.Categoria = produto.Categoria;
+                }
+
+            }
+
+            _context.SaveChanges();
+        }
+
+
+        //Excluir
+
+        public Produto Excluir(int produtoId)
+        {
+
+            Produto prod = _context.Produtos.Find(produtoId);
+
+            if (prod != null)
+            {
+                _context.Produtos.Remove(prod);
+              // _context.SaveChanges();
+            }
+
+            return prod;
+        }
     }
 }
